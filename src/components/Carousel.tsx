fa42cd0c7c8ba762bd0ab3f4ebf748bbab6f3f98
brianaqp_ <brianqp9889@gmail.com>
2025-08-21 14:14:30 -0600

    feat: transitions into carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/global.css";
+// import "./general.css";
+
+enum Moving {
+    STATIC,
+    MOVING
+}
 
 export default function() {
     const [image, setImageUrl] = useState("null");
+    const [monvingState, setMovingState] = useState(Moving.STATIC);
+
     const { LOCAL_S3_BUCKET } = import.meta.env;
 
     const URL = `${LOCAL_S3_BUCKET}images/big-beach.jpg`
 
+    useEffect(() => {
+
+    }, [monvingState])
+
+    // Trigger interval
+    useEffect(() => {
+        const TIME: Readonly<Number> = 500;
+        const timer = setInterval(() => {
+            
+        }, 500);
+
+        // Return clean up function
+        return () => clearInterval(timer);
+    }, []) // Run once
+
     return (
         <>
         <div className="w-full h-full relative">
-            <div id="image-container">
-                <img src={URL} alt="Image" />
-            </div>
-            {/* Stick with the nearest father */}
-            {/* First the background */}
-            <div className="absolute inset-0 bg-transparent hover:bg-gray-100 hover:opacity-10 z-100" />
+                <img src={URL} alt="Image" className="relative left-0 z-0 transition-all ease-in-out duration-1000"/>
 
-            {/* Bottom description */}
-            <div className="absolute bottom-0 inset-x-0 h-16 z-0 bg-blue-950 text-white">
-                <span>This is my text description</span>
-            </div>
+
+                {/* Bottom description */}
+                <div className="absolute bottom-0 inset-x-0 h-16 z-0 bg-blue-950 text-white">
+                    <span>This is my text description</span>
+                </div>
+
+                {/* Left buttom */}
+                <div className="absolute left-0 inset-y-16 w-16 bg-blue-950">
+
+                </div>
+
+                {/* Right buttom */}
+                <div className="absolute right-0 inset-y-16 w-16 bg-blue-950">
+                </div>
         </div>
         </>
     )
