7ed6d50b20ade37efc798824d77b8603712f8060
brianaqp_ <brianqp9889@gmail.com>
2025-08-22 16:42:37 -0600

    feat: state managment in carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,37 +1,72 @@
 import { use, useEffect, useRef, useState } from "react";
 import "../styles/global.css";
 
-enum CarouselState {
-    STATIC,
-    MOVING
-}
+const { PUBLIC_API } = import.meta.env;
 
-const { LOCAL_S3_BUCKET } = import.meta.env;
-const URL = `${LOCAL_S3_BUCKET}images/big-beach.jpg`
+enum Direction { RIGHT, LEFT }
+
+const images = [
+    "big-beach.jpg",
+    "dog.jpg"
+];
+
+const getUrlWithIndex = (index: number) => `${PUBLIC_API}images/${images[index]}`
 
 
 export default function() {
-    let [movingState, setMovingState] = useState(CarouselState.STATIC);
+    let [indexImage, setIndexImage] = useState(0);
+    let timerRef = useRef<number | null | NodeJS.Timeout>(null);
 
     useEffect(() => {
-        const TIME = 2000;
-        const timer = setInterval(() => {
-            setMovingState(previousState => 
-                previousState === CarouselState.STATIC ? CarouselState.MOVING : CarouselState.STATIC
-            );
-        }, TIME);
+        timerRef.current = setLocalInterval();
+ 
+        // Clean any interval in clean up
+        return () => {
+            if (timerRef.current != null) {
+                clearInterval(timerRef.current)
+            }
+        }
+    }, []);
 
-        return () => clearInterval(timer);
-    }, [])
+    const setLocalInterval = () => {
+        const TIME = 2000;
+
+        return setInterval(() => {
+            moveForward();
+        }, TIME);
+    }
+
+    const moveForward = () => {
+        setIndexImage(prev => (prev + 1 < images.length ? prev + 1 : 0));
+    };
+
+    const moveBackward = () => {
+        setIndexImage(prev => (prev - 1 >= 0 ? prev - 1 : images.length - 1))
+    }
+
+    const move = (direction: Direction) => {
+        if (timerRef.current) {
+            clearInterval(timerRef.current);
+            timerRef.current = setLocalInterval();
+        }
+        switch (direction) {
+            case Direction.LEFT:
+                moveBackward();
+                break;
+            case Direction.RIGHT:
+                moveForward();
+                break;
+        }
+    }
+
 
     return (
         <>
         <p></p>
         <div className="w-full h-full relative">
-                <img src={URL} 
+                <img src={getUrlWithIndex(indexImage)} 
                 alt="Image"
-                className={`relative left-0 z-0 transition-all ease-in-out duration-1000
-                ${movingState === CarouselState.MOVING ? "left-100" : "left"}`}
+                className={`relative left-0 z-0 transition-all ease-in-out duration-1000`}
                 />
 
                 {/* Bottom description */}
@@ -41,11 +76,12 @@ export default function() {
 
                 {/* Left buttom */}
                 <div className="absolute left-0 inset-y-16 w-16 bg-blue-950">
-
+                    <p onClick={() => move(Direction.LEFT)}>click me</p>
                 </div>
 
                 {/* Right buttom */}
                 <div className="absolute right-0 inset-y-16 w-16 bg-blue-950">
+                    <p onClick={() => move(Direction.RIGHT)}>click me</p>
                 </div>
         </div>
         </>
