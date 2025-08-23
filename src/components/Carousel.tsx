f613e7144e9fec8bab0cce275f6acf7d98f94d4d
brianaqp_ <brianqp9889@gmail.com>
2025-08-22 23:07:42 -0600

    refactor symbols and improve state

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -20,8 +20,10 @@ export default function() {
 
     let timerRef = useRef<number | null | NodeJS.Timeout>(null);
 
+    const value = <>hola!</>
+
     useEffect(() => {
-        timerRef.current = setLocalInterval();
+        timerRef.current = createLocalInterval();
  
         // Clean any interval/timer in clean up
         return () => {
@@ -31,48 +33,51 @@ export default function() {
         }
     }, []);
 
-    const setLocalInterval = () => {
+    const createLocalInterval = () => {
         const TIME = 2000;
-
         return setInterval(() => {
             move(Direction.RIGHT);
         }, TIME);
     }
 
-    // Change the symbol to mean the change of the image rather than the image movement. 
-    const moveForward = () => {
+    // Movement classes
+    const setNextImage = () => {
         setImageIndex(prev => (prev + 1 < images.length ? prev + 1 : 0));
     };
 
-    const moveBackward = () => {
+    const setPreviousImage = () => {
         setImageIndex(prev => (prev - 1 >= 0 ? prev - 1 : images.length - 1))
     }
 
-    // This functions needs to manage
-    // 1. Animation lifecycle
-    // 2. Image replacement
+    // 1. Omit if animation
+    // 2. Set the movement
+    // 3. Clear the interval
+    // 4. Timeout -> Change the image
     const move = (direction: Direction) => {
-        setAnimationState(AnimationState.MOVING);
+        if (animationState === AnimationState.MOVING) 
+            return;
 
-        setTimeout(() => {
+        setAnimationState(AnimationState.MOVING);
+        
         if (timerRef.current) {
             clearInterval(timerRef.current);
-            timerRef.current = setLocalInterval();
         }
+        
+        setTimeout(() => {
+            switch (direction) {
+                case Direction.LEFT:
+                    setPreviousImage();
+                    break;
+                case Direction.RIGHT:
+                    setNextImage();
+                    break;
+                default:
+                    break;
+            }
 
-        switch (direction) {
-            case Direction.LEFT:
-                moveBackward();
-                break;
-            case Direction.RIGHT:
-                moveForward();
-                break;
-            default:
-                break;
-        }
-
-        setAnimationState(AnimationState.IDLE)
-
+            setAnimationState(AnimationState.IDLE);
+            
+            timerRef.current = createLocalInterval();
         }, 1000);
     }
 
@@ -80,27 +85,33 @@ export default function() {
     return (
         <>
         <p></p>
-        <div className="w-full h-full relative">
-                <img src={getUrlWithIndex(imageIndex)} 
-                alt="Image"
-                className={`relative z-0 left-0 transition-all ease-in-out duration-1000 object-contain
-                    ${ animationState === AnimationState.MOVING ? "left-100" : "" }
-                    `}
-                />
-                {/* Bottom description */}
-                <div className="absolute bottom-0 inset-x-0 h-16 z-0 bg-blue-950 text-white">
-                    <span>This is my text description</span>
-                </div>
+        <div className="w-full h-full relative overflow-hidden">
 
-                {/* Left buttom */}
-                <div className="absolute left-0 inset-y-16 w-16 bg-blue-950">
-                    <p onClick={() => move(Direction.LEFT)}>click me</p>
-                </div>
+            {/* <img src={getUrlWithIndex(imageIndex)} 
+            className={`absolute -left-100 transition-all ease-in-out duration-1000 
+            ${ animationState === AnimationState.MOVING ? "left-0" : ""}`} 
+            /> */}
 
-                {/* Right buttom */}
-                <div className="absolute right-0 inset-y-16 w-16 bg-blue-950">
-                    <p onClick={() => move(Direction.RIGHT)}>click me</p>
-                </div>
+            <img src={getUrlWithIndex(imageIndex)} 
+            alt="Image"
+            className={`relative z-0 left-0 transition-all ease-in-out duration-1000 object-contain
+                ${ animationState === AnimationState.MOVING ? "opacity-0" : "opacity-100" }
+                `}
+            />
+            {/* Bottom description */}
+            <div className="absolute bottom-0 inset-x-0 h-16 z-0 bg-blue-950 text-white">
+                <span>This is my text description</span>
+            </div>
+
+            {/* Left buttom */}
+            <div className="absolute left-0 inset-y-16 w-16 bg-blue-950">
+                <p onClick={() => move(Direction.LEFT)}>click me</p>
+            </div>
+
+            {/* Right buttom */}
+            <div className="absolute right-0 inset-y-16 w-16 bg-blue-950">
+                <p onClick={() => move(Direction.RIGHT)}>click me</p>
+            </div>
         </div>
         </>
     )
