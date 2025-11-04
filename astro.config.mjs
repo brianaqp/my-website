37c236eb82f177fe89c1eb591ccaa61e152520e6
brian <brianqp9889@gmail.com>
2025-11-04 11:53:51 -0600

    fix: update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://brianaqp.github.io",
+  base: "/my-website",
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });
